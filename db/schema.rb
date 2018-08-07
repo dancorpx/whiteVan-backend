797cf5c59e3222858f2543bcbfda2e76b49f8b7e
Callum Tipper <callum.tipper@gmail.com>
2018-08-07 18:19:09 +0800

    the rails side of wechat login - finding or creating a user with open_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_07_022916) do
+ActiveRecord::Schema.define(version: 2018_08_07_095818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2018_08_07_022916) do
     t.string "area"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "open_id"
   end
 
 end
