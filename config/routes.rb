92370ec4f26503a212052ebf72babfe8269afea7
Callum Tipper <callum.tipper@gmail.com>
2018-08-07 20:17:16 +0800

    altered routes so login goes to correct path api/v1/login_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post '/login', to: 'login#login'
+
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      post '/login', to: 'login#login'
       resources :items, only: [ :index, :show, :update, :create, :destroy ]
       resources :exchanges, only: [:index, :show, :update, :create, :destroy]
       resources :users, only: [:update]
