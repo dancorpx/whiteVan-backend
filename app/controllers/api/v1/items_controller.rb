8a711c90cfa1197ef0351900c90098def39def92
Callum Tipper <callum.tipper@gmail.com>
2018-08-07 16:27:48 +0800

    crud actions and routes for Item

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,46 @@
+class Api::V1::ItemsController < Api::V1::BaseController
+  before_action :set_item, only: [:show, :update, :destroy]
+
+  def index
+    @items = Item.all
+  end
+
+  def show
+  end
+
+  def update
+    if @item.update(item_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
+  def destroy
+    @item.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category, :price, :quantity, :seller_id)
+  end
+
+  def render_error
+    render json: { errors: @item.errors.full_messages }, status: :unprocessable_entity
+  end
+end
