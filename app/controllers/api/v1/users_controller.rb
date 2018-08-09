0fac4c33edfb8a201221ad7bf4ccbddec2d0ed82
Callum Tipper <callum.tipper@gmail.com>
2018-08-09 15:42:47 +0800

    now there's a user side login to complete the user model just after weixin logs in

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,19 +1,21 @@
 class Api::V1::UsersController < Api::V1::BaseController
   def update
-    @user = User.find(:id)
-    if @user.update(user_params)
+    @user = User.find(params[:id])
+    if @user.update!(user_params)
       render json: @user
     else
       render_error
+    end
   end
 
   private
 
    def user_params
-    params.require(:user).permit(:id, :username, :theif_id, :area, :open_id)
+    params.require(:user).permit(:id, :username, :thief_id, :area, :open_id)
   end
 
   def render_error
     render json: { errors: @item.errors.full_messages }, status: :unprocessable_entity
   end
+
 end
